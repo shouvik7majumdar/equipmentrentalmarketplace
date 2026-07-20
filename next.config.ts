416d428236609382cf28394ff03d9480d7deadec
shouvik7majumdar <shouvikmajumdar204@gmail.com>
2026-07-21 00:37:13 +0530

    fix: adjust allowedDevOrigins position to root level in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["localhost", "10.166.42.139"],
   devIndicators: {
     appIsrStatus: false,
-  },
-  experimental: {
-    allowedDevOrigins: ["localhost", "10.166.42.139"],
   },
 };
 
